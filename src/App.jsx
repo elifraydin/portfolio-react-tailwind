0e3313a27360b6b93337c6c4af44b32b8501490d
elif rumeysa aydin <elifrumeysaaydin@gmail.com>
2022-10-02 22:10:52 +0300

    Dark Mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HeroSection from './components/HeroSection'
 
 function App() {
+
+
+  const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  }, []);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [theme]);
+
   return (
-    <div className='font-inter'>
-      <HeroSection/>
-    </div>
+    <>
+      <button
+        type="button"
+        onClick={handleThemeSwitch}
+        className="fixed z-10 right-2 top-2 bg-indigo-500 text-lg p-1 rounded-md"
+      >
+        {theme === 'dark' ? '🌙' : '🌞'}
+      </button>
+
+      <div className='font-inter bg:white dark:bg-slate-900'>
+        <HeroSection />
+      </div>
+    </>
   )
 }
 export default App
